68c0caa076e0ea829551a7f5c2e8b0f4fab630df
Jagrati Mathe <jagratimathe.jm@gmail.com>
2026-07-15 16:02:00 +0530

    change

--- 02_typecasting.py
+++ 02_typecasting.py
@@ -6,3 +6,14 @@ print(type(a))
 
 print(b)
 print(type(b))
+
+# convert b to an integer
+c=int(b)
+print(c)
+print(type(c))
+
+pi=3.14
+print(type(pi))
+int_pi=int(pi)
+print(type(int_pi)) 
+
---
+++ 03_input.py
@@ -0,0 +1,4 @@
+a=input("Enter a number:")
+b=input("Enter second number:")
+
+print(int(a)+3)
