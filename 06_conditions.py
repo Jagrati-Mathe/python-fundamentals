c1afec9196e49cbc736d8624bf99c000c3f7f8d1
Jagrati Mathe <jagratimathe.jm@gmail.com>
2026-07-16 17:55:30 +0530

    y

---
+++ 06_conditions.py
@@ -0,0 +1,16 @@
+# if_else condition
+# age = 12
+# if(age > 18):
+#     print("You can drive")
+#     print("Thank you")
+# print("end of program")
+
+# age= int(input("Enter Your Age:"))
+
+# if(age> 18):
+#     print("you can drive")
+# elif(age==18):
+#     print("let have an interview")
+# else: print("you cannot drive")
+
+
---
+++ 08_loops.py
@@ -0,0 +1,3 @@
+# range function goes to starting no to last-1
+for i in range(1,6):
+    print(i) 
