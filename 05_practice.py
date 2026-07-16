60b68e6bd408412ed50251f1aaee6aa6b9e3c4b0
Jagrati Mathe <jagratimathe.jm@gmail.com>
2026-07-16 13:05:10 +0530

    Create 05_practice.py

---
+++ 05_practice.py
@@ -0,0 +1,40 @@
+# print("Hello world! Welcome to python!")
+# print(f"Twinkle, Twinkle, little star,\nHow I wonder what you are!")
+
+#name="jagrati"
+#age=27
+#height=5.3
+##is_student=True
+#print("The name of the student is "+ name+ " and her age = "+ str(age) +" and height="+ str(#height) + " and she is student="+ str(is_student) )
+
+
+#num = 45
+#print(int(num) + 10)
+
+#food = input("What is your favorite food!?")
+#print("Wow I also like "+ food)
+
+
+# num1=20
+# num2=8
+# print(f"sum={num1 + num2}")
+# print(f"sub={num1 - num1}")
+# print(f"mul={num1 * num2}")
+# print(f"div={num1 / num2}")
+
+
+# a=int(input("Enter first number\n"))
+# b=int(input("Enter Second number\n"))
+
+# print("a + b is :", a + b)
+# print("a - b is :", a - b)
+# print("a * b is :", a * b)
+# print("a / b is :", a / b)
+
+a= int(input("Enter the number\n"))
+print("square is:",a**2)
+print("cube is:",a**3)
+
+
+
+
