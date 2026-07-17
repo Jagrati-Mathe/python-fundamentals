1444534d0a1481e794b68d1c4a3af305ae387ab8
Jagrati Mathe <jagratimathe.jm@gmail.com>
2026-07-17 16:44:03 +0530

    m

--- 08_loops.py
+++ 08_loops.py
@@ -1,3 +1,21 @@
 # range function goes to starting no to last-1
-for i in range(1,6):
-    print(i) 
+# for i in range(1,6):
+#     print(i) 
+
+# for i in range(1,11):
+#     print("2 X",i,"=",5*i)
+
+#while loop
+#i=1
+# while i<6:
+#     print(i)
+#     i=i+1
+
+for i in range(0,20):
+    print(i)
+    if(i==12):
+        continue
+    print("hi")
+
+
+
---
+++ 09_practice.py
@@ -0,0 +1,29 @@
+# num =int(input("Enter your number: "))
+
+# match num:
+#     case 1:
+#      print("Monday")
+#     case 2:
+#       print("Tuesday")
+#     case 3:
+#       print("Wednesday")
+#     case 4:
+#       print("Thursday")
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter Second number: "))
+
+# operations= input("Enter the operation: ")
+
+# match operations:
+#     case "+":
+#         print(num1+num2)
+#     case "-":
+#         print(num1-num2)
+#     case "*":
+#         print(num1*num2)
+#     case "/":
+#         print(num1/num2)
+
+
+
